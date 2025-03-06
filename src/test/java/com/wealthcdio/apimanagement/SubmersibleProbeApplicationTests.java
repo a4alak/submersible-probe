ae6f551c0fed9f0dc18d92dce32bcab70c45af67
alakesh <alakesh2006@gmail.com>
2025-03-06 14:04:01 +0530

    TestCases

--- src/test/java/com/wealthcdio/apimanagement/SubmersibleProbeApplicationTests.java
+++ src/test/java/com/wealthcdio/apimanagement/SubmersibleProbeApplicationTests.java
@@ -1,13 +1,66 @@
 package com.wealthcdio.apimanagement;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SubmersibleProbeApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	/**
+	 * Test Case:Probe should not go beyond the grid boundaries.
+	 */
 	@Test
-	void contextLoads() {
+	public void testGridboundaries() throws Exception {
+	}
+
+	/**
+	 * Test Case: Obstacle Avoidance - Probe should stop before hitting an obstacle.
+	 */
+	@Test
+	public void testStopBeforehittingAnObstacle() throws Exception {
+	}
+
+	/**
+	 * Test Case: Invalid Commands - API should return bad request for invalid
+	 * commands.
+	 */
+	@Test
+	public void testInvalidCommand() throws Exception {
+	}
+
+	/**
+	 * Test Case: Repeated Commands & Correct Tracking - Ensure that repeated moves
+	 * are tracked correctly.
+	 */
+	@Test
+	public void testRepeatedMoves() throws Exception {
+	}
+
+	/**
+	 * Test Case: Edge Grid Handling - Probe should handle movement when starting at
+	 * grid edges.
+	 */
+	@Test
+	public void testStartingatGridedges() throws Exception {
+	}
+
+	/**
+	 * Test Case: Full Rotation Ensure 360-degree rotation works correctly.
+	 */
+	@Test
+	public void testFullRotation() throws Exception {
 	}
 
 }
