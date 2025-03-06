548bd5a941d2e740effd91fba00d65ac6c1574a0
alakesh <alakesh2006@gmail.com>
2025-03-06 13:48:40 +0530

    project structure

---
+++ src/test/java/com/wealthcdio/apimanagement/SubmersibleProbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wealthcdio.apimanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmersibleProbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
